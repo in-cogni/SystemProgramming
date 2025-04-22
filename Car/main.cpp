12e66cd00b087aa722b93549450f87571d406f81
LadaKashubo <lkashubo@yandex.ru>
2025-04-22 20:36:11 +0300

    Engine DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -56,17 +56,28 @@ public:
 		cout << "Fuel level:\t" << fuel_level << " liters.\n";
 	}
 };
-
+#define MIN_ENGINE_CONSUMPTION 3
+#define MAX_ENGINE_CONSUMPTION 25
 class Engine
 {
-	double consumption_100, comsumption_sec;
-	bool run;
+	const double CONSUMPTION;//расход на 100 км
+	const double DEFAULT_CONSUMPTION_PER_SECOND;
+	double consumption_per_second;
+	bool is_started;
 public:
-	Engine(double consumpiton) :consumption_100(consumpiton), comsumption_sec(consumpiton / 1000000 * 30)
-		//пояснение к comsumption_sec(consumpiton / 1000000 * 30): 
-		//сначала нашла скарость из условия домашки (Если расход 10л/100км, то расход за одну секунду будет 0.0003 л/c)
-		//по форуле: расх(л/с) = ( расх(л/100км) / 1000000 ) * скорость(м/с) => скорость = 30 м/с
-		//и далее можем посчитать любой расход в л/с, зная расход в л/100км
+	double get_consumption_per_second()const
+	{
+		return consumption_per_second;
+	}
+	Engine(double consumption):
+		CONSUMPTION
+	    (
+			consumption < MIN_ENGINE_CONSUMPTION? MIN_ENGINE_CONSUMPTION:
+			consumption >MAX_ENGINE_CONSUMPTION?MAX_ENGINE_CONSUMPTION:
+			consumption
+		),
+		DEFAULT_CONSUMPTION_PER_SECOND(CONSUMPTION*3e-5),
+		consumption_per_second(DEFAULT_CONSUMPTION_PER_SECOND)
 	{
 		cout << "Engine is ready\n";
 	}
@@ -76,107 +87,149 @@ public:
 	}
 	void start()
 	{
-		run = true;
-		cout << "Engine is running\n";
+		is_started = true;
 	}
 	void stop()
 	{
-		run = false;
-		cout << "Engine is stopped\n";
+		is_started = false;
 	}
-	bool running() const
+	bool started()const
 	{
-		return run;
+		return is_started;
 	}
 	void info()const
 	{
-		cout << "\nEngine info:\n";
-		cout << "Consumption per 100 km:\t" << consumption_100 << " liters / 100 km\n";//расход л/100 км
-		cout << "Consumption per 1 sec:\t" << comsumption_sec << " liters / 1 sec\n";//расход л/с
-		cout << "Condition: "<< (running()?"running":"stopped") <<"\n";//состояние (запущен/остановлен)
+		cout << "Consumpiton: " << CONSUMPTION << " liters/100km\n";
+		cout << "Default Consumpiton: " << DEFAULT_CONSUMPTION_PER_SECOND << " liters/s\n";
+		cout << "Consumption: " << consumption_per_second << " liters/s\n";
 	}
 };
 
-class Car
-{
-	Tank tank;
-	Engine engine;
-public:
-	Car(int tank_capacity, double engine_consumption):tank(tank_capacity), engine(engine_consumption)
-	{
-		cout << "Car is ready\n";
-	}
-	~Car()
-	{
-		cout << "Car is over\n";
-	}
-	void fill(double amount)
-	{
-		tank.fill(amount);
-	}
-	void start_engine()
-	{
-		if (tank.get_fuel_level() > 0) engine.start();
-		else cout << "No fuel\n";
-	}
-	void stop_engine()
-	{
-		engine.stop();
-	}
-	void info() const
-	{
-		cout << "\n----Car info----\n";
-		tank.info();
-		engine.info();
-		cout << "\n-------------------\n\n";
-	}
-};
+//#define TANK_CHECK
+//#define ENGINE_CHECK
+//class Engine
+//{
+//	double consumption_100, comsumption_sec;
+//	bool run;
+//public:
+//	Engine(double consumpiton) :consumption_100(consumpiton), comsumption_sec(consumpiton / 1000000 * 30)
+//		//пояснение к comsumption_sec(consumpiton / 1000000 * 30): 
+//		//сначала нашла скарость из условия домашки (Если расход 10л/100км, то расход за одну секунду будет 0.0003 л/c)
+//		//по форуле: расх(л/с) = ( расх(л/100км) / 1000000 ) * скорость(м/с) => скорость = 30 м/с
+//		//и далее можем посчитать любой расход в л/с, зная расход в л/100км
+//	{
+//		cout << "Engine is ready\n";
+//	}
+//	~Engine()
+//	{
+//		cout << "Engine is over\n";
+//	}
+//	void start()
+//	{
+//		run = true;
+//		cout << "Engine is running\n";
+//	}
+//	void stop()
+//	{
+//		run = false;
+//		cout << "Engine is stopped\n";
+//	}
+//	bool running() const
+//	{
+//		return run;
+//	}
+//	void info()const
+//	{
+//		cout << "\nEngine info:\n";
+//		cout << "Consumption per 100 km:\t" << consumption_100 << " liters / 100 km\n";//расход л/100 км
+//		cout << "Consumption per 1 sec:\t" << comsumption_sec << " liters / 1 sec\n";//расход л/с
+//		cout << "Condition: "<< (running()?"running":"stopped") <<"\n";//состояние (запущен/остановлен)
+//	}
+//};
+//
+//class Car
+//{
+//	Tank tank;
+//	Engine engine;
+//public:
+//	Car(int tank_capacity, double engine_consumption):tank(tank_capacity), engine(engine_consumption)
+//	{
+//		cout << "Car is ready\n";
+//	}
+//	~Car()
+//	{
+//		cout << "Car is over\n";
+//	}
+//	void fill(double amount)
+//	{
+//		tank.fill(amount);
+//	}
+//	void start_engine()
+//	{
+//		if (tank.get_fuel_level() > 0) engine.start();
+//		else cout << "No fuel\n";
+//	}
+//	void stop_engine()
+//	{
+//		engine.stop();
+//	}
+//	void info() const
+//	{
+//		cout << "\n----Car info----\n";
+//		tank.info();
+//		engine.info();
+//		cout << "\n-------------------\n\n";
+//	}
+//};
 
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	/*Tank tank(80);
+#ifdef TANK_CHECK
+	Tank tank(80);
 	double fuel;
 	do
 	{
 		cout << "На сколько заправляемся?"; cin >> fuel;
 		tank.fill(fuel);
 		tank.info();
-	} while (true);*/
-
-	Car car(80, 10);//80 - вмсетимость бака, 10 - расход л/100км
-
-	double fuel;
-	short choice;
-
-	do
-	{
-		car.info();
-
-		cout << "Выберите:\n";
-		cout << "1 - заправиться\n";
-		cout << "2 - запустить двигатель\n";
-		cout << "3 - остановить двигатель\n";
-		cout << "0 - выход\n";
-
-		cin >> choice;
-
-		if (choice == 1)
-		{
-			cout << "На сколько заправляемся?\n"; cin >> fuel;//
-			car.fill(fuel);
-		}
-		else if (choice == 2)
-		{
-			car.start_engine();
-		}
-		else if (choice == 3)
-		{
-			car.stop_engine();
-		}
-		else if (choice == 0)
-			return;
-		else cout << "введено некорректое значение. повторите попытку\n";
 	} while (true);
-	
+#endif // TANK_CHECK
+
+#ifdef ENGINE_CHECK
+	Engine engine(10);
+	engine.info();
+#endif // ENGINE_CHECK
+
+
+	//Car car(80, 10);//80 - вмсетимость бака, 10 - расход л/100км
+	//double fuel;
+	//short choice;
+	//do
+	//{
+	//	car.info();
+	//	cout << "Выберите:\n";
+	//	cout << "1 - заправиться\n";
+	//	cout << "2 - запустить двигатель\n";
+	//	cout << "3 - остановить двигатель\n";
+	//	cout << "0 - выход\n";
+	//	cin >> choice;
+	//	if (choice == 1)
+	//	{
+	//		cout << "На сколько заправляемся?\n"; cin >> fuel;//
+	//		car.fill(fuel);
+	//	}
+	//	else if (choice == 2)
+	//	{
+	//		car.start_engine();
+	//	}
+	//	else if (choice == 3)
+	//	{
+	//		car.stop_engine();
+	//	}
+	//	else if (choice == 0)
+	//		return;
+	//	else cout << "введено некорректое значение. повторите попытку\n";
+	//} while (true);
+	//
 }
